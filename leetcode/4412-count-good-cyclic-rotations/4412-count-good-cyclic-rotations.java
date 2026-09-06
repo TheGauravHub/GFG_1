010e3302f1bce5dd4d8f1429aa527bdc6f79ae64
GAURAV KUMAR <gkchaudhary58@gmail.com>
2026-09-06 16:35:41 +0530

    Time: 3 ms (96.77%), Space: 71.4 MB (79.33%) - CodeHub

--- leetcode/4412-count-good-cyclic-rotations/4412-count-good-cyclic-rotations.java
+++ leetcode/4412-count-good-cyclic-rotations/4412-count-good-cyclic-rotations.java
@@ -1,35 +1,33 @@
 class Solution {
     public int countGoodRotations(int[] nums) {
+
         int n = nums.length;
-        int half = n / 2;
-
-        long total = 0;
-        for (int x : nums) total += x;
-
-        int[] arr = new int[2 * n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = nums[i];
-            arr[i + n] = nums[i];
-        }
-
-        long window = 0;
-
-        for (int i = 0; i < half; i++) {
-            window += arr[i];
-        }
-
         int ans = 0;
 
-        for (int start = 0; start < n; start++) {
+        long sum1 = 0;
+        long sum2 = 0;
 
-            if (2 * window > total) {
+        for (int i = 0; i < n / 2; i++) {
+            sum1 += nums[i];
+        }
+
+        for (int i = n / 2; i < n; i++) {
+            sum2 += nums[i];
+        }
+
+        for (int i = 0; i < n; i++) {
+
+            if (sum1 > sum2) {
                 ans++;
             }
 
-            if (start + half < 2 * n) {
-                window -= arr[start];
-                window += arr[start + half];
-            }
+            int mid = (i + (n / 2)) % n;
+
+            sum1 += nums[mid];
+            sum2 -= nums[mid];
+
+            sum1 -= nums[i];
+            sum2 += nums[i];
         }
 
         return ans;
